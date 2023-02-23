2996bc3f6f992a246c7df246465bc37b1167c73f
Vendy <124166758+ud4d@users.noreply.github.com>
2023-02-23 12:47:56 +0100

    Update streamlit_app.py
    add filter (loc fc)

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #List to pick fruit to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)), ['Avocado','Strawberries']
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
 #display the table on page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
