ba896eff860754291382c81122e7543e83fb4c3e
Vendy <124166758+ud4d@users.noreply.github.com>
2023-02-23 12:32:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text(':) Omega 3 & Blueberry Oatmeal')
 streamlit.text('Hard-Boiled Free-Range Egg')
